8f6dc9fc650b7226871d00050becff5aca66ffe3
lijin <18810996718@163.com>
2018-10-20 10:58:15 +0800

    add vuex

---
+++ vuex/src/store/plugins.js
@@ -0,0 +1,7 @@
+import { STORAGE_KEY } from './mutations'
+
+const localStoragePlugin = store => {
+  store.subscribe((mutation, { todos }) => {
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  })
+}
