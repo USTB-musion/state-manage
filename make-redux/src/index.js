a8aff29eb7dac7cea2595fccbf6e7954ad7d6be6
lijin <18810996718@163.com>
2018-10-22 21:15:58 +0800

    动手实现一个redux：优雅地修改共享状态

---
+++ make-redux/src/index.js
@@ -0,0 +1,49 @@
+// 应用的共享状态
+let appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
+  }
+}
+
+// 专门负责数据的修改，所有对数据的操作必须经过dispatch函数
+function dispatch (action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      appState.title.text = action.text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      appState.title.color = action.color
+      break
+    default:
+      break
+  }
+}
+
+// 把状态的数据渲染到页面上
+function renderApp (appState) {
+  renderTitle(appState.title)
+  renderContent(appState.content)
+}
+
+function renderTitle (title) {
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = title.text
+  titleDOM.style.color = title.color
+}
+
+function renderContent (content) {
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = content.text
+  contentDOM.style.color = content.color
+}
+
+renderApp(appState) // 首次渲染页面
+dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
+dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'red' }) // 修改标题颜色
+dispatch({ type: 'UPDATE_TITLE_TEXT', text: 'redux'}) // 修改标题文本
+renderApp(appState) // 把新的数据渲染到页面上
