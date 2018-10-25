9173f1243adbc5606a547f380c12c8bed1df3450
lijin <18810996718@163.com>
2018-10-25 08:21:26 +0800

    支持mapDispatchToProps

--- make-react-redux/src/ThemeSwitch.js
+++ make-react-redux/src/ThemeSwitch.js
@@ -1,48 +1,45 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from './react-redux'
 
 class ThemeSwitch extends Component {
-    static contextTypes = {
-      store: PropTypes.object
-    }
-  
-    constructor () {
-      super()
-      this.state = { themeColor: '' }
-    }
-  
-    componentWillMount () {
-      const { store } = this.context
-      this._updateThemeColor()
-      store.subscribe(() => this._updateThemeColor())
-    }
-  
-    _updateThemeColor () {
-      const { store } = this.context
-      const state = store.getState()
-      this.setState({ themeColor: state.themeColor })
-    }
+  static propTypes = {
+    themeColor: PropTypes.string,
+    onSwitchColor: PropTypes.func
+  }
 
-    handleSwitchColor(color) {
-        const { store } = this.context
-        store.dispatch({
-            type: 'CHANGE_COLOR',
-            themeColor: color
-        })
-    }
-  
-    render () {
-      return (
-        <div>
-          <button 
-            style={{ color: this.state.themeColor }}
-            onClick={this.handleSwitchColor.bind(this, 'red')}>Red</button>
-          <button 
-            style={{ color: this.state.themeColor }}
-            onClick={this.handleSwitchColor.bind(this, 'blue')}>Blue</button>
-        </div>
-      )
+  handleSwitchColor (color) {
+    if (this.props.onSwitchColor) {
+      this.props.onSwitchColor(color)
     }
   }
+
+  render () {
+    return (
+      <div>
+        <button
+          style={{ color: this.props.themeColor }}
+          onClick={this.handleSwitchColor.bind(this, 'red')}>Red</button>
+        <button
+          style={{ color: this.props.themeColor }}
+          onClick={this.handleSwitchColor.bind(this, 'blue')}>Blue</button>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSwitchColor: (color) => {
+      dispatch({ type: 'CHANGE_COLOR', themeColor: color })
+    }
+  }
+}
+ThemeSwitch = connect(mapStateToProps, mapDispatchToProps)(ThemeSwitch)
 
 export default ThemeSwitch
--- make-react-redux/src/react-redux.js
+++ make-react-redux/src/react-redux.js
@@ -1,38 +1,45 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-export const connect = (mapStateToProps) => (WrappedComponent) => {
-  class Connect extends Component {
-    static contextTypes = {
-      store: PropTypes.object
-    }
-
-    constructor () {
-      super()
-      this.state = { allProps: {} }
-    }
-
-    componentWillMount () {
-      const { store } = this.context
-      this._updateProps()
-      store.subscribe(() => this._updateProps())
-    }
-
-    _updateProps () {
-      const { store } = this.context
-      let stateProps = mapStateToProps(store.getState(), this.props) // 额外传入 props，让获取数据更加灵活方便
-      this.setState({
-        allProps: { // 整合普通的 props 和从 state 生成的 props
-          ...stateProps,
-          ...this.props
+export const connect = (mapStateToProps, mapDispatchToProps) => (WrappedComponent) => {
+    class Connect extends Component {
+      static contextTypes = {
+        store: PropTypes.object
+      }
+  
+      constructor () {
+        super()
+        this.state = {
+          allProps: {}
         }
-      })
+      }
+  
+      componentWillMount () {
+        const { store } = this.context
+        this._updateProps()
+        store.subscribe(() => this._updateProps())
+      }
+  
+      _updateProps () {
+        const { store } = this.context
+        let stateProps = mapStateToProps
+          ? mapStateToProps(store.getState(), this.props)
+          : {} // 防止 mapStateToProps 没有传入
+        let dispatchProps = mapDispatchToProps
+          ? mapDispatchToProps(store.dispatch, this.props)
+          : {} // 防止 mapDispatchToProps 没有传入
+        this.setState({
+          allProps: {
+            ...stateProps,
+            ...dispatchProps,
+            ...this.props
+          }
+        })
+      }
+  
+      render () {
+        return <WrappedComponent {...this.state.allProps} />
+      }
     }
-
-    render () {
-      return <WrappedComponent {...this.state.allProps} />
-    }
+    return Connect
   }
-
-  return Connect
-}
