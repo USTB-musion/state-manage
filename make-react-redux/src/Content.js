5ac64bb191d28b447861445b7b3b5edd4e58a38c
lijin <18810996718@163.com>
2018-10-25 08:03:14 +0800

    加上connect和mapStateToProps

--- make-react-redux/src/Content.js
+++ make-react-redux/src/Content.js
@@ -1,37 +1,28 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ThemeSwitch from './ThemeSwitch'
+import { connect } from './react-redux'
 
 class Content extends Component {
-    static contextTypes = {
-      store: PropTypes.object
-    }
-  
-    constructor () {
-      super()
-      this.state = { themeColor: '' }
-    }
-  
-    componentWillMount () {
-        const { store } = this.context
-        this._updateThemeColor()
-        store.subscribe(() => this._updateThemeColor())
-    }
-  
-    _updateThemeColor () {
-      const { store } = this.context
-      const state = store.getState()
-      this.setState({ themeColor: state.themeColor })
-    }
-  
-    render () {
-      return (
-        <div>
-          <p style={{ color: this.state.themeColor }}>这是内容</p>
-          <ThemeSwitch />
-        </div>
-      )
-    }
+  static propTypes = {
+    themeColor: PropTypes.string
   }
+
+  render () {
+    return (
+      <div>
+        <p style={{ color: this.props.themeColor }}>这是内容</p>
+        <ThemeSwitch />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+Content = connect(mapStateToProps)(Content)
 
 export default Content
--- make-react-redux/src/Header.js
+++ make-react-redux/src/Header.js
@@ -1,33 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from './react-redux'
 
 class Header extends Component {
-    static contextTypes = {
-      store: PropTypes.object
-    }
-  
-    constructor () {
-      super()
-      this.state = { themeColor: '' }
-    }
-  
-    componentWillMount () {
-        const { store } = this.context
-        this._updateThemeColor()
-        store.subscribe(() => this._updateThemeColor())
-    }
-  
-    _updateThemeColor () {
-      const { store } = this.context
-      const state = store.getState()
-      this.setState({ themeColor: state.themeColor })
-    }
-  
-    render () {
-      return (
-        <h1 style={{ color: this.state.themeColor }}>这是标题</h1>
-      )
-    }
+  static propTypes = {
+    themeColor: PropTypes.string
   }
+
+  render () {
+    return (
+      <h1 style={{ color: this.props.themeColor }}>这是标题</h1>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+Header = connect(mapStateToProps)(Header)
 
 export default Header
